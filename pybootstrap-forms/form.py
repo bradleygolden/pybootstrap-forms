462c0a7e8e2b7a0145b41431b71a8ee67f001e0c
Peter Snyder <snyderp@gmail.com>
2013-03-03 17:22:21 -0600

    Add initial unit tests, form generation, etc.

--- pybootstrap-forms/form.py
+++ pybootstrap-forms/form.py
@@ -1,8 +1,16 @@
 class Form(object):
 
-    def __init__(self, name=None, *args):
+    def __init__(self, name, *args):
         self.name = name
-        self.fields = args
+
+        # Create two mappings of the given fields, one as a list (to maintain
+        # order) and the second as a dict (to make querying by field name
+        # quick)
+        self.fields_ordered = args
+        fields_named = dict()
+        for field in args:
+            fields_named[field.name] = field
+        self.fields_named = fields_named
 
     def render(self):
         groups = (field.render_group() for field in self.fields)
@@ -14,3 +22,47 @@ class Form(object):
             </fieldset>
         """ % (legend, "\t\n".join(groups))
 
+    def populate(self, values):
+        """Populates the values in the form with a set of values.  If a value
+        is passed in as a value that doesn't correspond to an underlying
+        field, it is silently ignored.
+
+        Args:
+            values -- a dict of field name -> values mappings
+        """
+        for name, value in values.items():
+            if name in self.fields_named:
+                self.fields_named[name].value = value
+
+    def values(self):
+        """Returns a mapping of all values in the form and their coresponding
+        value.  Note that these values should not be trusted / used
+        utill they're checked with self.validate() first.
+
+        Return:
+            A dict mapping of all values in the form from the name of a
+            field to its corresponding value
+        """
+        return {field.name: field.value for field in self.fields_ordered}
+
+    def empty(self):
+        """Removes all values from the form, so that the structure is as given,
+        but all the fields have no value.  Note that this may result in an
+        invalid form state for valid fields"""
+        for field in self.fields_ordered:
+            field.value = None
+
+    def validate(self):
+        """Attempts to valdiate the given value of each field.
+
+        Return:
+            True if all fields passed validation, and otherwise False
+        """
+        is_valid = True
+        for field in self.fields_ordered:
+            is_valid = is_valid and field.validate()
+        return is_valid
+
+    @property
+    def fields(self):
+        return self.fields_ordered
--- pybootstrap-forms/inputs.py
+++ pybootstrap-forms/inputs.py
@@ -13,13 +13,21 @@ def _value_to_id(name, val):
 
 class Field(object):
 
-    def __init__(self, name, label=None, help=None, current=None, attrs=None):
+    def __init__(self, name, label=None, help=None, value=None, attrs=None, required=False):
+        self.required = required
+        self.errors = []
         self.name = name
         self.field_type = "text"
         self.label = label
         self.attrs = attrs
         self.help = help
-        self.current = current
+        self.value = value or ""
+
+    def value_sanatized(self):
+        if not self.value:
+            return ''
+        else:
+            return self.value.replace(r'"', '&quot;')
 
     def attrs_string(self):
         if self.attrs is None:
@@ -28,75 +36,158 @@ class Field(object):
             return " ".join(("%s='%s'" % (k, v) for k, v in self.attrs.items()))
 
     def render_label(self):
-        return '<label for="%s">%s</label>' % (self.name, self.label) if self.label else ""
+        if not self.label:
+            return ""
+        else:
+            return '<label class="control-label" for="%s">%s</label>' % (
+                self.name, self.label)
+
+    def render_errors(self):
+        if len(self.errors) == 1:
+            return ""
+        else:
+            template = '<span class="help-inline">%s</span>'
+            return "\n".join(template % (error,) for error in self.errors)
+
+    def render_classes(self):
+        classes = []
+        if self.required:
+            classes.append("required")
+        if len(classes) == 0:
+            return ''
+        else:
+            return "class='%s'" % (" ".join(classes),)
 
     def render_field(self):
-        return '<input type="%s" id="%s" name="%s" %s>' % (
-            self.field_type, self.name, self.name, self.attr_string())
+        return '<input type="%s" id="%s" name="%s" value="%s" %s %s>' % (
+            self.field_type, self.name, self.name, self.value_sanatized(),
+            self.attrs_string(), self.render_classes())
 
     def render_description(self):
-        if self.help:
-            return '<span class="help-block">%s</span>' % (self.help,)
-        else:
+        if not self.help:
             return ""
+        else:
+            return '<span class="help-block">%s</span>' % (self.help,)
 
     def render_group(self):
-        params = (self.render_label(), self.render_field(),
+        is_in_error = len(self.errors) > 1
+        params = ("error" if is_in_error else "", self.render_label(),
+                  self.render_field(), self.render_errors(),
                   self.render_description())
         return """
-            <div class="control-group">
+            <div class="control-group %s">
                 %s
-                %s
+                <div class="controls">
+                    %s
+                    %s
+                </div>
                 %s
             </div>""" % params
+
+    def validate(self):
+        """Returns a boolean description of whether the current value of the
+        field should be treated as valid.  Should return True if the
+        value if valid, and otherwise False.  If False, this method
+        should populate the self.errors list with descriptions of the problems
+
+        Return:
+            True if the value if valid, and otherwise False
+
+        """
+        if self.attrs and "maxlength" in self.attrs and len(self.value) > self.attrs['maxlength']:
+            error = "%s cannot be longer than %d characters" % (
+                self.name, self.attrs['maxlength'])
+            self.errors.append(error)
+        if self.required and (self.value is None or len(self.value) == 0):
+            error = "%s is a required field"
+            self.errors.append(error)
+        return len(self.errors) == 0
 
 
 class Checkbox(Field):
 
-    def __init__(self, name, value, **kwargs):
-        self.value = value
+    def __init__(self, name, allowed_value, **kwargs):
+        self.allowed_value = allowed_value
         super(Checkbox, self).__init__(name, **kwargs)
         self.label = None
+
+    def validate(self):
+        if self.value and self.value != self.allowed_value:
+            error = "%s is not a valid value for %s" % (
+                self.value, self.name)
+            self.errors.append(error)
+        return super(Checkbox, self).validate()
 
     def render_field(self):
         template = """
             <label class="checkbox">
-                <input type="checkbox" name="%s" id="%s" value="%s" %s %s>
+                <input type="checkbox" name="%s" id="%s" value="%s" %s %s %s>
                 %s
             </label>"""
-        return template % (self.name, self.name, self.value, "checked='checked'" if self.current == self.value else "", self.attr_string(), self.label)
+        params = (self.name, self.name, self.value,
+                  "checked='checked'" if self.value == self.allowed_value else "",
+                  self.attrs_string(), self.label, self.render_classes())
+        return template % params
 
 
 class TextArea(Field):
 
-    def __init__(self, name, rows=3, current="", **kwargs):
+    def __init__(self, name, rows=3, **kwargs):
         self.rows = rows
-        super(TextArea, self).__init__(name, current=current, **kwargs)
+        super(TextArea, self).__init__(name, **kwargs)
+
+    def validate(self):
+        return True
+
+    def value_sanatized(self):
+        if not self.value:
+            return ''
+        else:
+            return self.value.replace(r'<', '&lt;').replace(r'>', '&gt;')
 
     def render_field(self):
-        return '<textarea id="%s" name="%s" rows="%d" %s>%s</textarea>' % (
-            self.name, self.name, self.rows, self.attr_string(), self.current)
+        return '<textarea id="%s" name="%s" rows="%d" %s %s>%s</textarea>' % (
+            self.name, self.name, self.rows, self.attrs_string(),
+            self.render_classes(), self.value_sanatized())
 
 
 class Multiple(Field):
 
-    def __init__(self, name, values, **kwargs):
-        self.values = values
+    def __init__(self, name, value_pairs, **kwargs):
+        self.value_pairs = value_pairs
         super(Multiple, self).__init__(name, **kwargs)
 
 
-class Dropdown(Multiple):
+class Exclusive(Multiple):
+
+    def allowed_values(self):
+        return (value for (value, display) in self.value_pairs)
+
+    def validate(self):
+        """For fields that take a set number of values, and accept only one
+        of them, for validation just check to make sure that that the current
+        value is one of the allowed values."""
+        if self.value and self.value not in self.allowed_values():
+            error = "%s is not a valid value for %s" % (
+                self.value_sanatized(), self.name)
+            self.errors.append(error)
+        return super(Exclusive, self).validate()
+
+
+class Dropdown(Exclusive):
 
     def render_field(self):
-        options = ("<option value='%s' %s>%s</option>" % (val, "selected='selected'" if val == self.current else "", label) for val, label in self.values)
+        options = ("<option value='%s' %s>%s</option>" % (val, "selected='selected'" if val == self.value else "", label) for (val, label) in self.value_pairs)
+        params = (self.name, self.name, self.attrs_string(),
+                  self.render_classes(), "\t\n".join(options))
         return """
-            <select name="%s" id="%s" %s>
+            <select name="%s" id="%s" %s %s>
                 %s
             </select>
-        """ % (self.name, self.name, self.attr_string(), "\t\n".join(options))
+        """ % params
 
 
-class Radios(Multiple):
+class Radios(Exclusive):
 
     def render_field(self):
         template = """
@@ -105,5 +196,5 @@ class Radios(Multiple):
                 %s
             </label>
         """
-        radios = (template % (self.name, _value_to_id(self.name, val), val, "checked='checked'" if val == self.current else "", label) for (val, label) in self.values)
+        radios = (template % (self.name, _value_to_id(self.name, val), val, "checked='checked'" if val == self.value else "", label) for (val, label) in self.value_pairs)
         return "\n".join(radios)
